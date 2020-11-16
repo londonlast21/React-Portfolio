cecd7d0917f4272a8824a456aca47237d7c47ff2
jack <biffisbuffm@gmail.com>
2020-11-16 15:35:36 -0600

    portfolio

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,7 +7,7 @@ function ContactForm() {
             <h3>Email: londonlast21@gmail.com</h3>
             <a href="mailto:londonlast21@gmail.com">Click here to send me an email!</a>
             <br></br>
-            <h3>Phone: (760) 468-0XXX </h3>
+            <h3>Phone: (760) 468-0299 </h3>
             <br></br>
 
 
